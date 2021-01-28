c038ceb8e6b7a28fb201392a8e52ac35425ae98b
qa-ihussain <ihussain@qa.com>
2021-01-28 22:00:45 +0000

    changes to code

--- src/main/java/com/qa/rest/LibraryController.java
+++ src/main/java/com/qa/rest/LibraryController.java
@@ -1,5 +1,63 @@
 package com.qa.rest;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.qa.persistence.domain.Library;
+import com.qa.persistence.dto.LibraryDTO;
+import com.qa.services.LibraryService;
+
+@RestController
+@RequestMapping("/library")
 public class LibraryController {
 
+	private LibraryService service;
+
+	@Autowired
+	public LibraryController(LibraryService service) {
+		super();
+		this.service = service;
+	}
+
+// CREATE BOOK
+	@PostMapping("/addBook")
+	public LibraryDTO addBook(@RequestBody Library book) {
+		return this.service.addBook(book);
+	}
+
+// READ BOOK(S)
+	@GetMapping("/viewAll")
+	public List<LibraryDTO> getAllBooks() {
+		return this.service.getAllBooks();
+	}
+	
+	@GetMapping("/viewBook/{id}")
+	public ResponseEntity<LibraryDTO> readBook(@PathVariable("id") Long id) {
+		return ResponseEntity.ok(this.service.readOne(id));
+	}
+
+// UPDATE BOOK
+	@PutMapping("updateBook/{id}")
+	public ResponseEntity<LibraryDTO> update(@PathVariable("id") Long id,  @RequestBody Library model) {
+		return new ResponseEntity<>(this.service.updateBook(id, model), HttpStatus.ACCEPTED);
+	}
+	
+// DELETE BOOK
+	@DeleteMapping("/delete/{id}")
+	public ResponseEntity<LibraryDTO> removeBook(@PathVariable("id") Long id){
+		return this.service.removeBook(id) ? 
+				new ResponseEntity<>(HttpStatus.NO_CONTENT) :
+				new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+	}
 }
---
+++ src/main/resources/data-dev.sql
@@ -0,0 +1,9 @@
+ INSERT INTO user (first_name, last_name, age, user_name, password) 
+VALUES ('Claude', 'Duvalier', 26, 'cd52', 'password'), 
+('Tasha', 'Lee', 21, 'tlee', 'password'),
+('Levi', 'Ackerman', 30, 'lackerman', 'password');
+
+INSERT INTO library (book_title, author, pages, borrowed_books, available)
+VALUES ('The Midnight Library', 'Matt Haig', 304, 1, 5),
+('Extraterrestrial: The First Sign of Intelligent Life Beyond Earth', 'Avi Loeb', 240, 0, 1),
+('Kakegurui - Compulsive Gambler - Vol. 1', 'Homura Kawamoto and Toru Naomura', 240, 2, 0);
